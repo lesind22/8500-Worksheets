77649f0abe571745eea8f9963cb518d5dcf0cc55
Indira Lessington <ilessin@g.clemson.edu>
2025-03-28 19:14:35 -0400

    halfway through script

--- Web Script.qmd.r
+++ Web Script.qmd.r
@@ -25,18 +25,129 @@ library(tidyverse)
     ia_keyword_search("The Crisis Publishing Company", num_results = 12) %>% 
     ia_get_items() %>% 
     ia_metadata() %>% 
-    filter(field == "json") %>% 
+    filter(field == "txt") %>% 
     select(value)
 
 # The goal is to download the files from Internet Archive and then compile it all into a data frame. 
 
+library(dplyr)
+    dir <- tempdir()
+    ia_search(ats_query, <- c("publisher" = "The Crisis Publishing Company", "year" = "1930")) %>% 
+    ia_get_items() %>% 
+    ia_files() %>% 
+    filter(type == "pdf") %>% 
+    group_by(id) %>% 
+    slice(1) %>% 
+    ia_download(dir = dir, overwrite = FALSE) %>% 
+    glimpse()
+
+# Here I wanted to put all of the items into one data frame. (Consultation of Copilot after
+# the exhaustion of trial and error)
+    library(tidyverse)
+    identifiers <- c("sim_crisis_1930-01_37_1", 
+        "sim_crisis_1930-08_37_8",  
+        "sim_crisis_1930-02_37_2", 
+        "sim_crisis_1930-03_37_3",
+        "sim_crisis_1930-05_37_5",
+        "sim_crisis_1930-11_37_11",
+        "sim_crisis_1930-07_37_7",
+        "sim_crisis_1930-10_37_10",
+        "sim_crisis_1930-12_37_12",
+        "sim_crisis_1930-04_37_4",
+        "sim_crisis_1930-06_37_6",
+        "sim_crisis_1930-09_37_9"
+
+    )
+        metadata_list <- list ()
+            for (id in identifiers) {
+                item <- ia_get_items(item)
+                metadata_list[[id]] <- metadata
+            }
+    metadata_df <- bind_rows(metadata_list, .id = "identifier")
+    print(metadata_df)
+
+# It appears that the dataframe is empty so it is my job to create a function for 
+# all 12 of these files to be transfered to it. 
+
+library(internetarchive)
+library(tidyverse)
+
+identifiers <- c(
+  "sim_crisis_1930-01_37_1", 
+  "sim_crisis_1930-08_37_8",  
+  "sim_crisis_1930-02_37_2", 
+  "sim_crisis_1930-03_37_3",
+  "sim_crisis_1930-05_37_5",
+  "sim_crisis_1930-11_37_11",
+  "sim_crisis_1930-07_37_7",
+  "sim_crisis_1930-10_37_10",
+  "sim_crisis_1930-12_37_12",
+  "sim_crisis_1930-04_37_4",
+  "sim_crisis_1930-06_37_6",
+  "sim_crisis_1930-09_37_9"
+)
+
+metadata_list <- list()
+
+for (id in identifiers) {
+  item <- ia_get_items(id)
+  metadata <- ia_metadata(item) 
+  metadata_list[[id]] <- metadata 
+}
+    metadata_df <- bind_rows(metadata_list, .id = "identifier")
+    print(metadata_df)
+
+# Now the data frame is created, now I want to expand the data frame to include more columns.
+# Which also calls for another "for" statement
+
+identifiers <- c(
+  "sim_crisis_1930-01_37_1", 
+  "sim_crisis_1930-08_37_8",  
+  "sim_crisis_1930-02_37_2", 
+  "sim_crisis_1930-03_37_3",
+  "sim_crisis_1930-05_37_5",
+  "sim_crisis_1930-11_37_11",
+  "sim_crisis_1930-07_37_7",
+  "sim_crisis_1930-10_37_10",
+  "sim_crisis_1930-12_37_12",
+  "sim_crisis_1930-04_37_4",
+  "sim_crisis_1930-06_37_6",
+  "sim_crisis_1930-09_37_9"
+)
+
+    metadata_list <- list()
+
+    for (id in identifiers) {
+        item <- ia_get_items(id)
+        metadata <- ia_metadata(item)
+    }
+
+        metadata_list[[id]] <- tibble(
+            identifier = id,
+            title = metadata$TheCrisisMagazine %||%
+            creator = metadata$TheCrisisPublishingCompany %||% 
+            date = metadata$1930 %||%
+            language = metadata$english %||%
+        )
+    metadata_df <- bind_rows(metadata_list)
+    print(metadata_df)
+
+# In this last section of the script I will be downloading pdf files of the 12 Crisis Magazine that were retrieved. 
+
+    
+
+        
 
 
 
 
 
-    filter(field == "1930") %>% 
-    print(data.frame("sim_crisis_1930-07_37_7")
+
+
+
+
+
+
    
 
 
@@ -53,9 +164,6 @@ library(tidyverse)
 
 
 
-    pubcrisis <- ia_get_items("TheCrisisPublishingCompany")
-    ia_metadata()
-    ia_files()
 
 
 
@@ -64,39 +172,9 @@ library(tidyverse)
 
 
 
+       
 
-
-        ia_keyword_search("sim_crisis_1930-07_37_7", num_results = 12) %>% 
-        ia_get_items() %>% 
-        ia_metadata() %>% 
-    
+   
         
-        
-
-
-# Fix area below too!!!!
-library("internetarchive")
-        ia_get_items("sim_crisis_1930-12_37_12")
-        ia_metadata("sim_crisis_1930-12_37_12")
-        ia_files("sim_crisis_1930-12_37_12")
-
-
-#Fix area below before turning in Web Script!!!!!!
-    result <- ia_get_items("sim_crisis_1930-05_37_5")
-    result <- ia_get_items("sim_crisis_1930-03_37_3")
-    result <- ia_get_items("sim_crisis_1930-01_37_1")
-    result <- ia_get_items("sim_crisis_1930-08_37_8")
-        ia_metadata()
-        ia_files()
-
-   library("internetarchive")
-        result <- ia_get_items("sim_crisis_1930-07_37_7")
-        result <- ia_get_items("sim_crisis_1930-10_37_10")
-        result <- ia_get_items("sim_crisis_1930-02_37_2")
-        result <- ia_get_items("sim_crisis_1930-04_37_4")
-        result <- ia_get_items("sim_crisis_1930-06_37_6")
-        result <- ia_get_items("sim_crisis_1930-09_37_9")
-        ia_metadata()
-        ia_files()
 
        
